4c1091cea224ebef483b225bbb56d13e73aa902f
Malanichev Aleksey <malanichevab@gmail.com>
2023-04-04 14:38:21 +0300

    readme, settings

--- rdg0_site/rdg0_site/settings.py
+++ rdg0_site/rdg0_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-l_-^z6zvw#1qm+4j3ror7)sl62^9ihu+u_0(n!d=yt6zn^2@xe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '194.87.210.36',
+]
 
 
 # Application definition
@@ -118,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
